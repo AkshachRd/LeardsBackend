56cbcef5077d5a99c4c86dd4fd506f1aea94bb40
Daniil <dr.chashka@gmail.com>
2021-02-03 20:40:57 +0300

    Тест синхронизации

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from functions import is_dictionary_empty
 parser = WiktionaryParser()
 parser.set_default_language('english')
 
-print(parser.fetch('asdasaфsda')[0])
+print(parser.fetch('awsdasaфsda')[0])
 
 
 print(is_dictionary_empty(parser.fetch('turn on')[0]))
