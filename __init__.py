0a1616afef50c35ad5501e8fc403816ca9ed96f6
Daniil <dr.chashka@gmail.com>
2021-03-06 01:52:17 +0300

    Добавлена поддержка переменных окружения. Добавлено подключение к базе данных. Добавлена автоматическая установка пакетов при обнавлении репозитория на GitHub.

--- __init__.py
+++ __init__.py
@@ -1,13 +1,26 @@
 from flask import Flask, redirect, url_for, request
 from flask_cors import CORS, cross_origin
 from wordParser import parse_word
+from flask_sqlalchemy import SQLAlchemy
+from settings import *
 import json
 import git
 import os
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{db_username}:{db_password}@{db_hostname}/{db_name}".format(
+        db_username=DB_USERNAME, db_password=DB_PASSWORD, db_hostname=DB_HOSTNAME, db_name=DB_NAME
+    )
+    app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+    app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db = SQLAlchemy(app)
+
     cors = CORS(app, resources={r"/": {"origins": "*"}})
     app.config['CORS_HEADERS'] = 'Content-Type'
     app.config.from_mapping(
@@ -36,15 +49,14 @@ def create_app(test_config=None):
             word = request.json['word']
             return json.dumps(parse_word(word))
 
-
     @app.route('/webhook', methods=['POST'])
     @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
     def webhook():
         if request.method == 'POST':
             repo = git.Repo('./LeardsBackend')
             origin = repo.remotes.origin
-
             origin.pull()
+            os.system('cmd /c "python -m pip install -r requirements.txt"')
 
             return 'Updated PythonAnywhere successfully', 200
         else:
---
+++ settings.py
@@ -0,0 +1,6 @@
+import os
+
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOSTNAME = os.getenv("DB_HOSTNAME")
+DB_NAME = os.getenv("DB_NAME")
