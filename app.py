730503d44eced8dbe740a7995e3895779f3e3ef4
Daniil <dr.chashka@gmail.com>
2021-03-07 22:17:57 +0300

    Багфикс моделей БД

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from models import *
+from sqlalchemy.dialects.mysql import INTEGER
 from flask import Flask, redirect, url_for, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
@@ -21,6 +21,46 @@ app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "user"
+
+    id_user = db.Column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
+    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(255), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    phone = db.Column(db.String(20))
+
+
+class Deck(db.Model):
+    __tablename__ = "deck"
+
+    id_deck = db.Column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
+    background = db.Column(db.String(255), nullable=False)
+
+
+class Card(db.Model):
+    __tablename__ = "card"
+
+    id_card = db.Column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
+    front_text = db.Column(db.String(255), nullable=False)
+    back_text = db.Column(db.String(255), nullable=False)
+
+
+class Deck_has_card(db.Model):
+    __tablename__ = "deck_has_card"
+
+    id_deck = db.Column(INTEGER(unsigned=True), db.ForeignKey("deck.id_deck"))
+    id_card = db.Column(INTEGER(unsigned=True), db.ForeignKey("card.id_card"))
+
+
+class User_has_deck(db.Model):
+    __tablename__ = "user_has_deck"
+
+    id_user = db.Column(INTEGER(unsigned=True), db.ForeignKey("user.id_user"))
+    id_deck = db.Column(INTEGER(unsigned=True), db.ForeignKey("deck.id_deck"))
+
 
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config.from_mapping(
