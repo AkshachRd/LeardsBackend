ec308bbe1c97958aa8f582ef4fe3f81bdfae56c4
Daniil <dr.chashka@gmail.com>
2021-03-07 20:53:32 +0300

    Багфикс

--- __init__.py
+++ __init__.py
@@ -42,11 +42,11 @@ def create_app(test_config=None):
         pass
 
     # blueprint for auth routes in our app
-    from .auth import auth as auth_blueprint
+    from auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
     # blueprint for non-auth parts of app
-    from .main import main as main_blueprint
+    from main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
