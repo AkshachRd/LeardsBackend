8c3c48eb916aaed5a8d0825dfc79d529a1a385a7
Daniil <dr.chashka@gmail.com>
2021-04-24 18:25:18 +0300

    Багфикс моделей

--- models/user_has_deck.py
+++ models/user_has_deck.py
@@ -4,5 +4,5 @@ from my_sqlalchemy import db
 class UserHasDeck(db.Model):
     __tablename__ = 'user_has_deck'
 
-    id_user = db.Column(db.String(255), db.ForeignKey("user.id_user"), primary_key=True)
+    id_user = db.Column(db.Integer, db.ForeignKey("user.id_user"), primary_key=True)
     id_root_deck = db.Column(db.String(255), db.ForeignKey("deck.id_deck"), primary_key=True)
