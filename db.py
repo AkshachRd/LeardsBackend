0af613f13edde35a0e9df1ea9274138fc0ec16e2
Daniil <dr.chashka@gmail.com>
2021-04-26 22:07:08 +0300

    Дебаг файла подлючения БД.

--- db.py
+++ db.py
@@ -1,12 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 import app
 
-# SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{db_username}:{db_password}@{db_hostname}/{db_name}".format(
-# db_username=app.app.config['DB_USERNAME'], db_password=app.app.config['DB_PASSWORD'], db_hostname=app.app.config[
-# 'DB_HOSTNAME'], db_name=app.app.config['DB_NAME'] )
-
-SQLALCHEMY_DATABASE_URI = "postgresql://akshachrd:6zM6IOoLDfV3P3vHqdmt@localhost:5432/leards_db"  # .format(
-#    db_username=DB_USERNAME, db_password=DB_PASSWORD, db_hostname=DB_HOSTNAME, db_name=DB_NAME
-#)
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{db_username}:{db_password}@{db_hostname}/{db_name}".format(
+    db_username=app.app.config['DB_USERNAME'], db_password=app.app.config['DB_PASSWORD'], db_hostname=app.app.config[
+        'DB_HOSTNAME'], db_name=app.app.config['DB_NAME'])
 
 db = SQLAlchemy()
