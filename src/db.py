64aa4cb49fb4386f7d4f0d5dede7d5bbef48b032
Daniil <dr.chashka@gmail.com>
2021-05-03 00:14:57 +0300

    Решена проблема подключения к БД

--- src/db.py
+++ src/db.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from src import app
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{db_username}:{db_password}@{db_hostname}/{db_name}".format(
     db_username=app.app.config['DB_USERNAME'], db_password=app.app.config['DB_PASSWORD'], db_hostname=app.app.config[
