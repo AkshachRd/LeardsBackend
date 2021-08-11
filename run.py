59c6cbf1c53f196f96ab4151fe68e6254a5ec4e9
Daniil <dr.chashka@gmail.com>
2021-08-11 06:10:09 +0300

    Env vars loading update

--- run.py
+++ run.py
@@ -1,2 +1,7 @@
-from app import app as flask_app
+from dotenv import load_dotenv
+from src.app import app as flask_app
+import os
+
+load_dotenv(os.path.join(os.getcwd(), ".env"))
+
 flask_app.run(debug=True)
