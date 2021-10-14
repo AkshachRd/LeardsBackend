62b794b5e0ee13c1d7a8845ac3a573919d3f7f84
Daniil <dr.chashka@gmail.com>
2021-10-15 00:15:07 +0300

    Новая модель таблиц БД: id изменен на uuid4. Соответсвенные изменения в приложение

--- src/models.py
+++ src/models.py
@@ -1,36 +1,37 @@
-from sqlalchemy import MetaData, Table, Column, Integer, String, ForeignKey
+from sqlalchemy import MetaData, Table, Column, String, ForeignKey
+import uuid
 
 metadata = MetaData()
 
 card = Table('card', metadata,
-             Column('id_card', Integer, autoincrement=True, primary_key=True),
+             Column('id_card', String(36), default=str(uuid.uuid4()), primary_key=True),
              Column('front_text', String(255), nullable=False),
              Column('back_text', String(255), nullable=False)
              )
 
 deck = Table('deck', metadata,
-             Column('id_deck', String(255), primary_key=True),
+             Column('id_deck', String(36), default=str(uuid.uuid4()), primary_key=True),
              Column('name', String(255), nullable=False),
              )
 
 deck_has_card = Table('deck_has_card', metadata,
-                      Column('id_deck', String(255), ForeignKey("deck.id_deck"), primary_key=True),
-                      Column('id_card', Integer, ForeignKey("card.id_card"), primary_key=True)
+                      Column('id_deck', String(36), ForeignKey("deck.id_deck"), primary_key=True),
+                      Column('id_card', String(36), ForeignKey("card.id_card"), primary_key=True)
                       )
 
 deck_has_deck = Table('deck_has_deck', metadata,
-                      Column('id_parent_deck', String(255), ForeignKey("deck.id_deck"), primary_key=True),
-                      Column('id_child_deck', String(255), ForeignKey("deck.id_deck"), primary_key=True)
+                      Column('id_parent_deck', String(36), ForeignKey("deck.id_deck"), primary_key=True),
+                      Column('id_child_deck', String(36), ForeignKey("deck.id_deck"), primary_key=True)
                       )
 
 user = Table('user', metadata,
-             Column('id_user', Integer, autoincrement=True, primary_key=True),
-             Column('username', String(64), index=True, unique=True, nullable=False),
+             Column('id_user', String(36), default=str(uuid.uuid4()), primary_key=True),
+             Column('username', String(64), index=True, nullable=False),
              Column('email', String(255), index=True, unique=True, nullable=False),
              Column('password_hash', String(128), nullable=False)  # TODO: character(n)
              )
 
 user_has_deck = Table('user_has_deck', metadata,
-                      Column('id_user', Integer, ForeignKey("user.id_user"), primary_key=True),
-                      Column('id_root_deck', String(255), ForeignKey("deck.id_deck"), primary_key=True)
+                      Column('id_user', String(36), ForeignKey("user.id_user"), primary_key=True),
+                      Column('id_root_deck', String(36), ForeignKey("deck.id_deck"), primary_key=True)
                       )
